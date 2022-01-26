b31f5224d3ba25fde6f6abf676cff4a7c06334f3
Marcelo Surco <marcelo.surco@utec.edu.pe>
2022-01-25 22:16:53 -0500

    Array con useState

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './components/Card'
 
-
+ 
 const App = ()=>{
   //Se regresan elementos de jsx
-  const numero = 1;
-  const frutas = ["Manzana", "Pera", "Naranja"];
-  return (<div>
-            <p>Hola</p> 
-            <Card titulo={`La tarjeta número ${numero}`} numero ={numero}/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            {frutas}
-            {frutas.map((frutas,index)=><p key={index}>{frutas}</p>)}
+  const [tarjetas, setTarjetas] = useState([])
+
+  const agregarTarjeta = () =>{
+    tarjetas.push(Card)
+    setTarjetas([...tarjetas])
+  }
+
+  const eliminarTarjeta = (id)=>{
+    const nuevasTarjetas = tarjetas.filter((Card, index)=>index!==id)
+    setTarjetas(nuevasTarjetas)
+  }
+
+  return (<div >
+            <button onClick={agregarTarjeta}>Agregar Tarjeta</button>
+            <div className = "cuadricula">
+            {tarjetas.map((Card,index)=><Card key={index} numero={index} eliminar={eliminarTarjeta} titulo={`Tarjeta número ${index}`}/>)}
+            </div>
+            
           </div>);
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,14 @@
 import React,{useState} from 'react';
-
+import "../css/Card.css"
 export default function Card(props) {
     const [numero, setNumero] = useState(0) //valor inicial
     const aumentar = ()=>{
         setNumero(numero + 1)
     }
     
-    return <>
+    return <div className = "card">
         <h2>{props.titulo}</h2>
+        <button onClick={()=>{props.eliminar(props.numero)}}>Eliminar</button>
         <p>Esta es una función Card</p>
         {/* Se coloca entre llaves, para tomarlo como una expresión de javascript */}
         <p>Contenido número {props.numero}</p>
@@ -15,5 +16,5 @@ export default function Card(props) {
         <p>Numero: {numero}</p>
         <button onClick={aumentar}>Aumentar</button>
         <button onClick={()=>{setNumero(0)}}>Reset</button>
-    </>;
+    </div>;
 }
